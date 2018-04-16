5c19cf987df9ae54996d863925a0bf04a9e8a2a7
yc5424tl <yc5424tl@go.minneapolis.edu>
2018-04-16 17:33:57 -0500

    commented out db config for test push to heroku

--- config.py
+++ config.py
@@ -12,22 +12,22 @@ class Config(object):
     SECRET_KEY = "testing_secret_key"
 
     # Database Config
-    DB_PROTOCOL = "postgresql+psycopg2"
-    DB_HOST = "localhost"
-    DB_PORT = "5432"
-    DB_USER = "lmnop"
-    DB_PASSWORD = os.getenv("LMNOP_DB_PW")
-    DB_NAME = "lmnop"
-    DB_TEMPLATE = '{protocol}://{user}:{pw}@{host}:{port}/{db}'
-
-    DB_CONN = DB_TEMPLATE.format(
-        protocol=DB_PROTOCOL,
-        user=DB_USER,
-        pw=DB_PASSWORD,
-        host=DB_HOST,
-        port=DB_PORT,
-        db=DB_NAME
-    )
+    # DB_PROTOCOL = "postgresql+psycopg2"
+    # DB_HOST = "localhost"
+    # DB_PORT = "5432"
+    # DB_USER = "lmnop"
+    # DB_PASSWORD = os.getenv("LMNOP_DB_PW")
+    # DB_NAME = "lmnop"
+    # DB_TEMPLATE = '{protocol}://{user}:{pw}@{host}:{port}/{db}'
+    #
+    # DB_CONN = DB_TEMPLATE.format(
+    #     protocol=DB_PROTOCOL,
+    #     user=DB_USER,
+    #     pw=DB_PASSWORD,
+    #     host=DB_HOST,
+    #     port=DB_PORT,
+    #     db=DB_NAME
+    # )
 
     # Celery Config
 
